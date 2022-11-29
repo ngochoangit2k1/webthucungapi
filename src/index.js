92c6d2326dc9d7266d3f056c5ccb4a57e60ad40b
ngochoangit2k1 <59520070+ngochoangit2k1@users.noreply.github.com>
2022-11-30 01:48:36 +0700

    make it better2

--- src/index.js
+++ src/index.js
@@ -39,6 +39,7 @@ app.use(cookieParser());
 
 app.use(function (req, res, next) {
   res.header("Content-Type", "application/json;charset=UTF-8");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Credentials", true);
   res.header(
     "Access-Control-Allow-Headers",
